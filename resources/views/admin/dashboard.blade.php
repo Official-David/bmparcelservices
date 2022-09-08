9beff4f6f55d7959fab950f428c472a919ca1a70
David Adigwu <davidadigwu@gmail.com>
2022-09-08 11:33:56 +0100

    refactor: display correct data from shipment model

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -19,37 +19,37 @@
                         <div class="col-xl-3 col-lg-6 col-sm-6 border-right">
                             <div class="card-body text-center">
                                 <h6 class="mb-3">Shipment Accepted</h6>
-                                <div class="chart-circle chart-circle-md" data-value="0.45" data-thickness="10"
+                                <div class="chart-circle chart-circle-md" data-value="{{$shipment->where('shipment_status', 'shipment accepted')->count()/100}}" data-thickness="7"
                                     data-color="#564ec1">
                                 </div>
-                                <h2 class="mb-1 mt-3  display-4 font-weight-semibold text-dark">6</h2>
+                                <h2 class="mb-1 mt-3  display-4 font-weight-semibold text-dark">{{$shipment->where('shipment_status', 'shipment accepted')->count()}}</h2>
                             </div>
                         </div>
                         <div class="col-xl-3 col-lg-6  col-sm-6 border-right">
                             <div class="card-body text-center">
                                 <h6 class="mb-3">In Transit</h6>
-                                <div class="chart-circle chart-circle-md" data-value="0.72" data-thickness="10"
+                                <div class="chart-circle chart-circle-md" data-value="{{$shipment->where('shipment_status', 'in transit')->count()/100}}" data-thickness="7"
                                     data-color="#04cad0">
                                 </div>
-                                <h2 class="mb-1 mt-3 display-4 font-weight-semibold text-dark">1</h2>
+                                <h2 class="mb-1 mt-3 display-4 font-weight-semibold text-dark">{{$shipment->where('shipment_status', 'in transit')->count()}}</h2>
                             </div>
                         </div>
                         <div class="col-xl-3 col-lg-6 col-sm-6 border-right">
                             <div class="card-body text-center">
                                 <h6 class="mb-3">Out For Delivery</h6>
-                                <div class="chart-circle chart-circle-md" data-value="0.38" data-thickness="10"
+                                <div class="chart-circle chart-circle-md" data-value="{{$shipment->where('shipment_status', 'out for delivery')->count()/100}}" data-thickness="7"
                                     data-color="#f5334f">
                                 </div>
-                                <h2 class="mb-1 mt-3  display-4 font-weight-semibold text-dark">8</h2>
+                                <h2 class="mb-1 mt-3  display-4 font-weight-semibold text-dark">{{$shipment->where('shipment_status', 'out for delivery')->count()}}</h2>
                             </div>
                         </div>
                         <div class="col-xl-3 col-lg-6  col-sm-6">
                             <div class="card-body text-center">
                                 <h6 class="mb-3">Delivered</h6>
-                                <div class="chart-circle chart-circle-md" data-value="0.03" data-thickness="10"
+                                <div class="chart-circle chart-circle-md" data-value="{{$shipment->where('shipment_status', 'delivered')->count()/100}}" data-thickness="7"
                                     data-color="#f7b731">
                                 </div>
-                                <h2 class="mb-1 mt-3  display-4 font-weight-semibold text-dark">397</h2>
+                                <h2 class="mb-1 mt-3  display-4 font-weight-semibold text-dark">{{$shipment->where('shipment_status', 'delivered')->count()}}</h2>
                             </div>
                         </div>
                     </div>
