f65141005a95c994661d5a5acd1415f7f958c448
David Adigwu <davidadigwu@gmail.com>
2022-09-08 11:32:49 +0100

    perf: send model to view

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Shipment;
+use App\Models\Shipment_Notification;
 
 class AdminController extends Controller
 {
     public function index(){
-        return view('admin.dashboard');
+        $shipment = Shipment::get();
+        return view('admin.dashboard', compact('shipment'));
     }
 }
