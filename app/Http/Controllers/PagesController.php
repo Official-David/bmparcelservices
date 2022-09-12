19138f2bc98eab08c624f27ee215379ccfe24459
David Adigwu <davidadigwu@gmail.com>
2022-09-12 11:47:55 +0100

    perf: sent all shipment info to the track result page

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Receiver;
+use App\Models\Sender;
 use Illuminate\Http\Request;
 use App\Models\Shipment;
 use App\Models\Shipment_Notification;
@@ -26,9 +28,11 @@ class PagesController extends Controller
     }
     public function trackShipment(Request $request){
         $shipments = Shipment::where('tracking_id', $request->input('tracking_id'))->get();
+        $senderInfo = Sender::where('shipment_id', $shipments[0]->id)->get();
+        $receiverInfo = Receiver::where('shipment_id', $shipments[0]->id)->get();
         if($shipments->count() > 0){
             $shipmentDetails = Shipment_Notification::where('shipment_id', $shipments[0]->id)->get();
-            return view('guest.track-result', compact('shipments', 'shipmentDetails'));
+            return view('guest.track-result', compact('shipments', 'shipmentDetails', 'senderInfo', 'receiverInfo'));
         }else{
             return back();
         }
